43e2a809d79d2b50bd7540ba0e83f5ebd2c1ad89
Janis Ringli <janis.ringli@gmail.com>
2022-07-07 09:39:51 +0200

    init

---
+++ src/main/java/ch/bbw/jr/zvvticketterminal/ZvvTicketTerminalApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.jr.zvvticketterminal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZvvTicketTerminalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZvvTicketTerminalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/bbw/jr/zvvticketterminal/ZvvTicketTerminalApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.jr.zvvticketterminal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZvvTicketTerminalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
